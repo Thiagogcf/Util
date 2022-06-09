cb0d12cac08a792f07ddfd371b4ccdef811d3719
Thiago <Thiagogferronato@gmail.com>
2022-06-09 11:02:45 -0300

    Sei la vei

--- Desenho.py
+++ Desenho.py
@@ -2,13 +2,20 @@ import time
 
 import pyautogui
 from PIL import Image
-im = Image.open('Img\Screenshot_2.png') # Can be many different formats.
+
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+filename = askopenfilename()
+print(filename)
+im = Image.open(filename) # Can be many different formats.
 pix = im.load()
 pyautogui.PAUSE = 0.001
 x,y = im.size  # Get the width and hight of the image for iterating over
-for h in range(0,x,1):
-    for w in range(0,y,1):
+for h in range(0,x,3):
+    for w in range(0,y,3):
         if pix[h,w] < (100, 100, 100, 255):
             #pyautogui.moveTo(h+100,w+100)
             pyautogui.click(h+300,w+300)
-            print(pix[h,w])  # Get the RGBA Value of the a pixel of an image 
+            print(pix[h,w])  # Get the RGBA Value of the a pixel of an image
---
+++ asci.py
@@ -0,0 +1,33 @@
+import pyautogui
+from PIL import Image
+
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+Tk().withdraw()
+filename = askopenfilename()
+print(filename)
+im = Image.open(filename) # Can be many different formats.
+pix = im.load()
+x,y = im.size  # Get the width and hight of the image for iterating over
+f = open('readme.txt',"w")
+f.truncate(0)
+for h in range(0,x,5):
+    for w in range(0,y,5):
+        if pix[h,w] < (50, 50, 50, 255):
+            print(' @ ', end='')
+            f.write(' @ ')
+        elif pix[h,w] < (100, 100, 100, 255):
+            print(' % ', end='')
+            f.write(' % ')
+        elif pix[h,w] < (150, 150, 150, 255):
+            print(' # ', end='')
+            f.write(' # ')
+        elif pix[h,w] < (200, 200, 200, 255):
+            print(' * ', end='')
+            f.write(' * ')
+        else:
+            print('   ', end='')
+            f.write('   ')
+    print("\n", end='')
+    f.write("\n")
+f.close()
