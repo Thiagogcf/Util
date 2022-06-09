fdf09d91018fde51a0e5ee00a3151234023d7435
Thiago <Thiagogferronato@gmail.com>
2022-06-09 09:49:37 -0300

    Sei la vei

--- Desenho.py
+++ Desenho.py
@@ -2,14 +2,13 @@ import time
 
 import pyautogui
 from PIL import Image
-time.sleep(5)
-im = Image.open('Img\Screenshot_1.png') # Can be many different formats.
+im = Image.open('Img\Screenshot_2.png') # Can be many different formats.
 pix = im.load()
-pyautogui.PAUSE = 0
+pyautogui.PAUSE = 0.001
 x,y = im.size  # Get the width and hight of the image for iterating over
-for h in range(x):
-    for w in range(y):
-        if pix[h,w] < (200, 200, 200, 255):
+for h in range(0,x,1):
+    for w in range(0,y,1):
+        if pix[h,w] < (100, 100, 100, 255):
             #pyautogui.moveTo(h+100,w+100)
-            pyautogui.click(h+100,w+100)
-            print(pix[h,w])  # Get the RGBA Value of the a pixel of an image
+            pyautogui.click(h+300,w+300)
+            print(pix[h,w])  # Get the RGBA Value of the a pixel of an image 
